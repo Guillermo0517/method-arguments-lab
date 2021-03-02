9659fe695eb9736931d113c90cd92ab4000efb76
Guillermo <guillermomemomarquez@gmail.com>
2021-03-01 22:29:33 -0800

    commit tree 197e9e2c8a0505fcd27541dbe670b3967d81f454

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+#introduction
+def introduction (name)
+    puts "Hi, my name is #{name="Josh"}."
+end
+
+#introduction_with_language
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name="Dan"} and I am learning to program in #{language="Ember.js"}."
+end
